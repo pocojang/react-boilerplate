50dd07106af7c607c3dd49e5964f6d25bd6a5487
devJang <devjang2016@gmail.com>
2019-06-23 16:01:44 +0900

    Update: full change

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,45 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(path.resolve(__dirname, 'src/js'), 'app.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      template: path.join(path.resolve(__dirname, 'src'), 'index.html'),
-    }),
-    new extractTextPlugin('style.bundle.css'),
-  ],
+  mode: 'development',
+  entry: './src/index.js',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
+        use: 'babel-loader',
         exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        loader: extractTextPlugin.extract({
-          use: ['css-loader'],
-        }),
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: ['file-loader'],
       },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    stats: {
+      color: true,
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ProgressBarPlugin({
+      format: 'Build [:bar] :percent (:elapsed seconds)',
+      clear: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
 };
